0e3eee36f8ddad9bc8a40f9b4a5b4203e52cdd7a
9486Phil <40156977+9486Phil@users.noreply.github.com>
2022-10-19 18:18:08 +0800

    Add files via upload

---
+++ LLX/Opencv_prac.py
@@ -0,0 +1,16 @@
+import numpy as num
+import cv2
+
+img = cv2.imread('Tiger.jpg')
+
+head = img[220:570,40:360].copy()
+cv2.rectangle(head,(0,0),(318,347),(0,255,0),3)
+
+for i in range(0,3):
+    for j in range(0,3):
+        img[i*350:(i+1)*350,j*320:(j+1)*320] = head
+
+cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
