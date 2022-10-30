60b63913883b4b17a37aef2b84cf96e49733fe80
霜月惊梦 <115629616+Frostyume@users.noreply.github.com>
2022-10-30 17:24:31 +0800

    Add files via upload

---
+++ lsk/HW03/MXMqtt.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+class MXMqtt:
+
+    def __init__(self, host, post):
+        self.host = host
+        self.post = post
+        self.mqttClient = mqtt.Client()
+        self.message = None
+        self.flag = False
+        self.__connect()
+
+    def __connect(self):
+        self.mqttClient.connect(self.host, self.post, 60)
+        self.mqttClient.loop_start()
+
+    def __messageBack(self, client, userdata, msg):
+        self.message = msg
+        self.flag = True
+
+    def PUB(self, topic, payload, qos=1):
+        """发布信息"""
+        self.mqttClient.publish(topic, payload, qos)
+
+    def SUB(self, topic, qos=1):
+        """订阅频道"""
+        self.mqttClient.subscribe(topic, qos)
+        self.mqttClient.on_message = self.__messageBack
+
+    def returnMsg(self):
+        """获取返回的消息"""
+        if self.flag:
+            self.flag = False
+            return self.message.payload.decode("utf-8"), self.message.topic
+        return None
+
