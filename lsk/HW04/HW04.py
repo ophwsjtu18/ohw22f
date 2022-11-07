8ac6a7eb3351cedccc0b3cb836e5ef103073a18e
霜月惊梦 <115629616+Frostyume@users.noreply.github.com>
2022-11-08 00:26:19 +0800

    Add files via upload

--- lsk/HW04/HW04.py
+++ lsk/HW04/HW04.py
@@ -4,7 +4,6 @@ from math import sqrt
 import mcpi.minecraft as minecraft
 import pyautogui as ui
 
-
 ui.FAILSAFE = False
 ui.PAUSE = 0.01
 LEFT = 1
@@ -27,23 +26,20 @@ def getDis(x1, y1, x2, y2):
 
 
 def getDrection(x1, y1, x2, y2):
-    if getDis(x1, y1, x2, y2) <= 10:
-        return STANDBY
-    else:
-        if abs(x1 - x2) < 30:
-            if y1 < y2 and getDis(x1, y1, x2, y2) >= 80:
-                #print("forward",getDis(x1, y1, x2, y2))
-                return FORWARD
-            elif y1 > y2 and getDis(x1, y1, x2, y2) >= 80:
-                #print("backward",getDis(x1, y1, x2, y2))
-                return BACKWARD
-        elif abs(y1 - y2) < 60:
-            if x1 < x2 and getDis(x1, y1, x2, y2) >= 60:
-                #print("left",getDis(x1, y1, x2, y2))
-                return LEFT
-            elif x1 > x2 and getDis(x1, y1, x2, y2) >= 60:
-                #print("right",getDis(x1, y1, x2, y2))
-                return RIGHT
+    if abs(x1 - x2) < 30:
+        if y1 < y2 and getDis(x1, y1, x2, y2) >= 80:
+            # print("forward",getDis(x1, y1, x2, y2))
+            return FORWARD
+        elif y1 > y2 and getDis(x1, y1, x2, y2) >= 80:
+            # print("backward",getDis(x1, y1, x2, y2))
+            return BACKWARD
+    elif abs(y1 - y2) < 60:
+        if x1 < x2 and getDis(x1, y1, x2, y2) >= 40:
+            # print("left",getDis(x1, y1, x2, y2))
+            return LEFT
+        elif x1 > x2 and getDis(x1, y1, x2, y2) >= 40:
+            # print("right",getDis(x1, y1, x2, y2))
+            return RIGHT
     return STANDBY
 
 
@@ -67,14 +63,14 @@ def hands2direction(img):
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-               # print(id,lm)
+                # print(id,lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
-                for i in [2, 4, 5, 8, 12, 16, 20]:
+                for i in [0, 2, 4, 5, 8, 12, 16, 20]:
                     if id == i:
                         x[i], y[i] = cx, cy
-                        #print(i,":",x[i],y[i])
+                        # print(i,":",x[i],y[i])
 
                 for a in [4, 12, 16, 20]:
                     for b in [4, 12, 16, 20]:
@@ -86,7 +82,6 @@ def hands2direction(img):
                 if flag:
                     dir = getDrection(x[8], y[8], x[5], y[5])
 
-
                 for a in [8, 12, 16, 20]:
                     for b in [8, 12, 16, 20]:
                         if getDis(x[a], y[a], x[b], y[b]) > 80:
@@ -96,8 +91,19 @@ def hands2direction(img):
                             flag = True
                 if (flag):
                     dir = getDrection(x[4], y[4], x[2], y[2])
+
     return dir
 
+
+def display(dir):
+    if dir == FORWARD:
+        cv2.putText(img, "forword", (x[8] - 20, y[8] - 20), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2)
+    if dir == BACKWARD:
+        cv2.putText(img, "backward", (x[8] - 20, y[8] + 40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2)
+    if dir == RIGHT:
+        cv2.putText(img, "right", (x[4] + 40, y[4]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2)
+    if dir == LEFT:
+        cv2.putText(img, "left", (x[4] - 40, y[4]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 2)
 
 
 def direction2move(dir):
@@ -135,6 +141,7 @@ while True:
     img = cv2.flip(frame, 1)
     findHands(img)
     dir = hands2direction(img)
+    display(dir)
     direction2move(dir)
     cv2.imshow("image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
